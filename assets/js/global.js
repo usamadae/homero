56a49c2b1046ca7eb64355ec7c37ba0d19d4c2c4
jalinahmed <152904125+jalinahmed@users.noreply.github.com>
2024-12-25 15:06:10 +0500

    live

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,22 +1,22 @@
 
-    tailwind.config = {
-      theme: {
-        extend: {
-          colors: {
-            
-            lightgrey: '#141F2F',
-            golden : '#F79B1E',
-            gray:'#959AA1',
-          },
-          fontFamily: {
-            poppins: ['Poppins', 'sans-serif'],
-          },
-          backgroundImage: {
-            'button-gradient': 'linear-gradient(to right, #F79B1E, #ED1945)',
-          },
-        }
-      }
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        
+        lightgrey: '#141F2F',
+        golden : '#F79B1E',
+        gray:'#959AA1',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      backgroundImage: {
+        'button-gradient': 'linear-gradient(to right, #F79B1E, #ED1945)',
+      },
     }
+  }
+}
 
 
     $('.logos-slider').slick({
