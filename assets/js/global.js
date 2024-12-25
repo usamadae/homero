227c09dc53bb2bd9e63b3a48bbbe077edc467927
usama usman <69691249+usamadae@users.noreply.github.com>
2024-12-25 14:58:10 +0500

    hero

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,43 +1,42 @@
 
-    tailwind.config = {
-      theme: {
-        extend: {
-          colors: {
-            
-            lightgrey: '#141F2F',
-            golden : '#F79B1E',
-            gray:'#959AA1',
-          },
-          fontFamily: {
-            poppins: ['Poppins', 'sans-serif'],
-          },
-          backgroundImage: {
-            'button-gradient': 'linear-gradient(to right, #F79B1E, #ED1945)',
-          },
-        }
-      }
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        
+        lightgrey: '#141F2F',
+        golden : '#F79B1E',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      backgroundImage: {
+        'button-gradient': 'linear-gradient(to right, #F79B1E, #ED1945)',
+      },
     }
+  }
+}
 
 
-    $('.logos-slider').slick({
-      slidesToShow: 7,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      arrows: false,
-      dots: false,
-      centerMode: true, // Enables centering
-      centerPadding: '0px',
-      pauseOnHover: false,
-      responsive: [{
-      breakpoint: 768,
-      settings: {
-      slidesToShow: 3
-      }
-      }, {
-      breakpoint: 520,
-      settings: {
-      slidesToShow: 2
-      }
-      }]
-      });
+$('.logos-slider').slick({
+  slidesToShow: 7,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: false,
+  dots: false,
+  centerMode: true, // Enables centering
+  centerPadding: '0px',
+  pauseOnHover: false,
+  responsive: [{
+  breakpoint: 768,
+  settings: {
+  slidesToShow: 3
+  }
+  }, {
+  breakpoint: 520,
+  settings: {
+  slidesToShow: 2
+  }
+  }]
+  });
