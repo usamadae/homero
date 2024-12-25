4e503d6bd4fec6e9a8cf635d14d6bd9b8456a82a
usama usman <69691249+usamadae@users.noreply.github.com>
2024-12-25 11:15:27 +0500

    jalib handover

---
+++ assets/js/global.js
@@ -0,0 +1,18 @@
+
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            
+            lightgrey: '#141F2F',
+            golden : '#F79B1E',
+          },
+          fontFamily: {
+            poppins: ['Poppins', 'sans-serif'],
+          },
+          backgroundImage: {
+            'button-gradient': 'linear-gradient(to right, #F79B1E, #ED1945)',
+          },
+        }
+      }
+    }
