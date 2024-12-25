e3b9fb3799dab59963319f77bf5a244f8815c02f
usama usman <69691249+usamadae@users.noreply.github.com>
2024-12-25 14:52:48 +0500

    push the code

--- assets/js/global.js
+++ assets/js/global.js
@@ -16,3 +16,27 @@
         }
       }
     }
+
+
+    $('.logos-slider').slick({
+      slidesToShow: 7,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      arrows: false,
+      dots: false,
+      centerMode: true, // Enables centering
+      centerPadding: '0px',
+      pauseOnHover: false,
+      responsive: [{
+      breakpoint: 768,
+      settings: {
+      slidesToShow: 3
+      }
+      }, {
+      breakpoint: 520,
+      settings: {
+      slidesToShow: 2
+      }
+      }]
+      });
